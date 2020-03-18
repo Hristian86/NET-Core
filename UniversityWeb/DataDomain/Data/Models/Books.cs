da228f7146dcfd0c7ccc2e62b5e8e00430eb3c0e
Hristian86 <59567820+Hristian86@users.noreply.github.com>
2020-03-18 13:30:37 +0200

    Adding more properties to collections
    Adding Rating, Description, and some validations

--- UniversityWeb/DataDomain/Data/Models/Books.cs
+++ UniversityWeb/DataDomain/Data/Models/Books.cs
@@ -12,9 +12,19 @@ namespace DataDomain.Data.Models
         }
 
         public int Id { get; set; }
+
+        [MaxLength(50)]
         public string Title { get; set; }
+
+        [MaxLength(30)]
         public string Author { get; set; }
+
+        [MaxLength(50)]
         public string Genre { get; set; }
+
+        [MaxLength(100)]
+        public string Actors { get; set; }
+
         public string UserId { get; set; }
 
         [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
@@ -27,6 +37,10 @@ namespace DataDomain.Data.Models
 
         [DisplayFormat(DataFormatString = "{0:f2}", ApplyFormatInEditMode = true)]
         public double Discount { get; set; }
+        public double Raiting { get; set; }
+
+        [MaxLength(150)]
+        public string Description { get; set; }
 
         public virtual ICollection<Rentals> Rentals { get; set; }
     }
--- UniversityWeb/DataDomain/Data/Models/Movies.cs
+++ UniversityWeb/DataDomain/Data/Models/Movies.cs
@@ -16,14 +16,22 @@ namespace DataDomain.Data.Models
         [MaxLength(50)]
         public string Title { get; set; }
 
-        [MaxLength(50)]
+        [MaxLength(30)]
         public string Director { get; set; }
+
+        [MaxLength(150)]
+        public string Description { get; set; }
 
         [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
         public DateTime RealeaseDate { get; set; }
+
         public DateTime Created { get; set; } = DateTime.UtcNow;
+
         public int? RentMovieId { get; set; }
+
         public string Picture { get; set; }
+
+        [MaxLength(50)]
         public string Genre { get; set; }
 
         [DisplayFormat(DataFormatString = "{0:f2}", ApplyFormatInEditMode = true)]
