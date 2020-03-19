b530f74b264cd7e71487d1311bf9103e3ae3829a
Hristian86 <59567820+Hristian86@users.noreply.github.com>
2020-03-19 11:13:47 +0200

    Adding httppost action to purchasing a movie

--- UniversityWeb/WebAppProject/Controllers/ShopingController.cs
+++ UniversityWeb/WebAppProject/Controllers/ShopingController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using BusinessLogic.interfaces;
+using BusinessLogic.OutputModels;
+using DataDomain.Data.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,10 +14,13 @@ namespace WebAppProject.Controllers
     public class ShopingController : Controller
     {
         private readonly IViewMovies _movieDb;
+        private readonly IShopItems _shoping;
 
-        public ShopingController(IViewMovies movieDb)
+        public ShopingController(IViewMovies movieDb,
+            IShopItems shoping)
         {
             this._movieDb = movieDb;
+            this._shoping = shoping;
         }
 
         public IActionResult Index()
@@ -40,6 +46,54 @@ namespace WebAppProject.Controllers
             }
 
             return View(movies);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Purchase(int id, [Bind("Id,Title,Director,RealeaseDate,Genre,price,Discount,Picture,Actors,Raiting,Description")] Movieses movies)
+        {
+
+
+            var user = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+
+
+
+
+            if (id != movies.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+
+                    //this._shoping.BuyMovie(user, movies.Id);
+
+
+                }
+
+                catch (InvalidOperationException ex)
+                {
+                    if (!MoviesExists(movies.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(movies);
+        }
+
+        private bool MoviesExists(int id)
+        {
+            return _movieDb.GetListOfMovies().Any(x => x.Id == id);
         }
     }
 }
